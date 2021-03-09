28d9073b2f64c1a544f02dff9eb4fd8ab4dc07da
Tess Watt <71218139+Tess314@users.noreply.github.com>
2021-03-09 19:54:52 +0000

    Update techie.js

--- techie.js
+++ techie.js
@@ -1,1 +1,11 @@
-
+function darkMode()
+			{
+				document.body.style.backgroundColor = "black";	
+				document.getElementById("header").style.color = "white";
+			}
+			
+function lightMode()
+			{
+				document.body.style.backgroundColor = "white";	
+				document.getElementById("header").style.color = "black";
+			}
