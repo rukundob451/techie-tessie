3b83ed7ac3e7a8b303595bb0c3b3b02cf1866977
Tess Watt <71218139+Tess314@users.noreply.github.com>
2021-03-11 14:02:25 +0000

    Update techie.js

--- techie.js
+++ techie.js
@@ -2,6 +2,7 @@ function darkMode()
 			{
 				document.body.style.backgroundColor = "black";	
 				document.getElementById("paragraph").style.color = "white";
+				document.getElementByClassName("grid-item").style.backgroundColor = "#CEB2D6";
 			}
 			
 function lightMode()
